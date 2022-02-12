891ba11ce9cf7e47e9c9f93e565011b7d7909563
Oscar Andersson <oscar.966484@gmail.com>
2022-02-12 14:23:38 +0100

    refacor for use of eyetracker object

--- app.py
+++ app.py
@@ -8,83 +8,15 @@ import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
 import Sensors.e4 as e4
+from Sensors.eye_tracker import EyeTracker
 
-
-#app = dash.Dash(__name__)
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-
-def filter_by_date(date, df):
-    '''
-    input
-      df: dataframe with a column named timeobj, containing datetimeobbjects
-      date: datetime date, can be created, from datetime import date, date(2022, 1, 27) for example
-    output
-      dataframe filtered be the data
-    '''
-    df['date'] = df['timeobj'].apply(lambda x: x.date())
-    df = df[df['date'] == date.date()]
-    return df
-
-def eye_tracking_visualization(date):
-    ''' process data and creating a figure '''
-
-    def convert_to_dateformat(start, sec):
-        timechange = timedelta(seconds=sec)
-        return start + timechange
-
-    eyedata = 'data/eye_tracker/result/User 1_all_gaze.csv'
-    df = pd.read_csv(eyedata)
-
-    df = df[['TIME(2022/01/27 09:39:32.995)', 'TIMETICK(f=10000000)', 'FPOGX', 'FPOGY']]
-    start_time = df.columns[0]
-    timeobj = datetime.strptime(start_time, 'TIME(%Y/%m/%d %H:%M:%S.%f)')
-    df = df.rename(columns={'TIME(2022/01/27 09:39:32.995)': 'time'})
-    df['timeobj'] = df['time'].apply(lambda x: convert_to_dateformat(timeobj, x))
-
-    # the average time between records
-    test_df = df.iloc[:20]
-    diff = []
-    curr = test_df['time'][0]
-    for time in test_df['time'][1:]:
-        diff.append(time - curr)
-        curr = time
-    time_between_records = np.array(diff).mean()
-
-    # remove some records
-    df = df[df.index % 40 == 0]
-    df = filter_by_date(date, df)
-
-    # Change width and height to match screen size, or create variable for aspect ratio
-    fig = px.scatter(df,
-                 x='FPOGX',
-                 y='FPOGY',
-                 animation_frame='time',
-                 range_x=[0, 1],
-                 range_y=[1, 0],
-                 height=500,
-                 width=500
-    )
-    return fig
-
-
-def filter_by_date(date, df):
-    '''
-    input
-      df: dataframe with a column named timeobj, containing datetimeobbjects
-      date: datetime date, can be created, from datetime import date, date(2022, 1, 27) for example
-    output
-      dataframe filtered be the data
-    '''
-    df['date'] = df['timeobj'].apply(lambda x: x.date())
-    df = df[df['date'] == date.date()]
-    return df
-
+eye_tracker = EyeTracker()
 
 def e4_bvp_visualization():
     ''' process e4 data for bvp '''
     return e4.get_e4_bvp('../data/e4_wristband/session4/BVP.csv')
-
 
 # defining the graph outside the layout for easier read
 graph_card = dbc.Card(
@@ -112,8 +44,8 @@ app.layout = html.Div(
         Output('eye_tracking_visualization', 'figure'),
         Input('datepicker', 'date'))
 def update_func(date):
-    date = datetime.strptime(date, '%Y-%m-%d')
-    return eye_tracking_visualization(date)
+    date = datetime.strptime(date, '%Y-%m-%d').date()
+    return eye_tracker.fig(date)
 
 if __name__ == '__main__':
     app.run_server(host='localhost', debug=True)
