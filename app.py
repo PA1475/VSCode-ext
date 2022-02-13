2e278c546b913d69d754c2b812692e1988f90d6b
William Lebrato <william.lebrato@gmail.com>
2022-02-13 15:38:36 +0100

    Changed the imports

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import dash
 import os
 from datetime import datetime, timedelta, date
 import numpy as np
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
