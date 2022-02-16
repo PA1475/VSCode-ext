65ef4dbfabf1df7291f76907ea0153d01961c2a4
Mans <mank20@student.bth.se>
2022-02-16 12:47:31 +0100

    Clean file E4Wristband class

--- Sensors/e4.py
+++ Sensors/e4.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import plotly.express as px
+from datetime import datetime
 
-def get_e4_bvp(path):
-    ''' read ey temp data from specified path '''
+def _clean_df(df):
+    time_zero_str = df.columns[0]
+    df = df.rename(columns={time_zero_str : "data"})
+    time_zero = float(time_zero_str)
+    frequency = int(df["data"].iloc[0])
+    df = df[1:]
+    df = df[df.index % frequency == 1]
+    print(df)
+    def _u_to_d(unixtime):
+        dt = datetime.utcfromtimestamp(
+            unixtime).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        return dt
+    df["time"] = [_u_to_d((i)+time_zero) for i in range(len(df["data"]))] 
+    return df
 
-    df = pd.read_csv(path)
-    time = list(df)[0]
-    hz = df[time][0]
 
-    df['timestamp'] = [(float(time)+item/hz) for item in range(-1, len(df.index)-1)]
-    df = df.iloc[1:]
-    fig = px.line(df, x='timestamp', y=time)
-
-    return fig
