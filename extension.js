b258e02d81dd2b48ab7b638b5a7a3d53dea4ac24
Jimmy Berglund <jimmy_berglund@hotmail.com>
2022-05-10 13:04:53 +0200

    Fixed so git hook will be added by vscode extension

--- extension.js
+++ extension.js
@@ -7,6 +7,8 @@ const net = require('net');
 const { write } = require('fs');
 const { privateEncrypt } = require('crypto');
 
+const GIT_HOOK_SCRIPT = "#!/bin/sh\nFILE=$1\nMESSAGE=$(cat $1)\nlines=`wc -l .git/logs/HEAD | awk '{print $1;}'`\nAUTHOR=`git config user.name`\nTIMESTAMP=`git log --author=\"$AUTHOR\" -1 --format=%ct`\nINPUT=\"emotions.csv.tmp\"\ncurl -o $INPUT \"127.0.0.1:8050/assets/emotions.csv\" 2>/dev/null\nTMP=(0 0 0 0)\nwhile IFS=\",\" read -r TIME EMOT\ndo\nTMP1=${EMOT%.*}\nTMP2=${TIME%.*}\nif [ \"$TMP1\" != \"emotions\" ]\nthen\nif [ $TMP2 -gt $TIMESTAMP ]\nthen\n((TMP[(($TMP1-1))]++))\nfi\nfi\ndone < $INPUT\nBIG=${TMP[0]}\nINDEX=0\nSUM=0\nfor i in {0..3}\ndo\nSUM=$((TMP[$i]+$SUM))\nif [ ${TMP[$i]} -gt $BIG ]\nthen\nBIG=${TMP[$i]}\nINDEX=$i\nfi\ndone\nrm $INPUT\nEMOTION=\" \"\nif [ $INDEX = 0 ]\nthen\nEMOTION=\"😰\"\nfi\nif [ $INDEX = 1 ]\nthen\nEMOTION=\"😃\"\nfi\nif [ $INDEX = 2 ]\nthen\nEMOTION=\"😞\"\nfi\nif [ $INDEX = 3 ]\nthen\nEMOTION=\"😐\"\nfi\nCERT=$((100 * $BIG/$SUM))\necho \"$MESSAGE\n\n$EMOTION $CERT%\" > $FILE"
+
 let statusbar_item;
 let e4_statusbar;
 let eye_statusbar;
@@ -16,14 +18,34 @@ let e4_connected = false;
 let eye_connected = false;
 let baseline = false;
 
-SUCCESS_STR = "SUCC"
-FAIL_STR = "FAIL"
+const SUCCESS_STR = "SUCC"
+const FAIL_STR = "FAIL"
 
 
 function displayMessage(message) {
 	// displays message as popup
 	vscode.window.showInformationMessage(message);
 }
+
+
+async function addGitHook() {
+	let rootFolder = vscode.workspace.workspaceFolders[0].uri;
+	let gitfolder = vscode.Uri.joinPath(rootFolder, ".git");
+	try {
+		let res = await vscode.workspace.fs.readDirectory(gitfolder);
+		let githook = vscode.Uri.joinPath(gitfolder, "hooks/commit-msg");
+		try {
+			res = await vscode.workspace.fs.stat(githook);
+		} catch(e) {
+			let answer = await vscode.window.showInformationMessage("It seems this is a git project, would you like to add the git hook?", "Yes", "No");
+			if (answer == "Yes") {
+				await vscode.workspace.fs.writeFile(githook, Buffer.from(GIT_HOOK_SCRIPT), {create: true, overwrite: false});
+			}
+		}
+	} catch(e) {}
+}
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 /**
@@ -34,6 +56,8 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "emotionawareide" is now active!');
+	
+	addGitHook();
 
 	// The command has been defined in the package.json file
 	let disposable = vscode.commands.registerCommand('emotionawareide.start_dashboard', show_web_view);
